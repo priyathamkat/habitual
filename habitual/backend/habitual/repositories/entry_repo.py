5fe29d089d48a8cefff7de34e6b3ebdd6ec8157e
Priyatham Kattakinda <priyathamkat@gmail.com>
2025-09-02 15:01:41 -0400

    Execute db write in a threadpool

--- habitual/backend/habitual/repositories/entry_repo.py
+++ habitual/backend/habitual/repositories/entry_repo.py
@@ -1,15 +1,17 @@
-from sqlmodel import Session
+from fastapi.concurrency import run_in_threadpool
 
+from habitual.db.session import get_session
 from habitual.models.entry import Entry
 
 
-def _save_entry(entry: Entry, session: Session) -> Entry:
-    session.add(entry)
-    session.commit()
-    session.refresh(entry)
-    return entry
+def _save_entry(entry: Entry) -> Entry:
+    with get_session() as session:
+        session.add(entry)
+        session.commit()
+        session.refresh(entry)
+        return entry
 
 
-async def create_entry_db(entry: Entry, session: Session) -> Entry:
+async def create_entry_db(entry: Entry) -> Entry:
     # Run blocking DB work in a thread to avoid blocking the event loop
-    return _save_entry(entry, session)
+    return await run_in_threadpool(_save_entry, entry)
